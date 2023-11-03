e58379c09e84b7f123c8c6cd13d39ca05569ba09
Joaograco4 <joaograco4@gmail.com>
2023-11-03 19:40:19 -0300

    uwu

--- app_base.cmd
+++ app_base.cmd
@@ -1,5 +1,6 @@
 echo off
 color 70
+:loop
 cls
 echo ==================================================================
 echo.
@@ -7,3 +8,14 @@ echo                        Android4Hack
 echo.
 echo.
 echo Options:
+echo (1) scrcpy         (3) Android Multi-tools
+echo (2) gnirehtet      (4) Donwload Mode
+ set /p "choice=>"
+echo ==================================================================
+pause
+if %choice% equ 1 (cd 1
+start scrcpy
+cls
+goto loop)
+if %choice% equ 2 (cd 2
+)
